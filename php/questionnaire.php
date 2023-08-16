fb29ab9c2e22c8a46be9a6a1fa62679ce8aedf99
gersheald <gersheald@gmail.com>
2023-08-16 20:17:59 +0300

    Add all project files to git

---
+++ php/questionnaire.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--метатег для масштабирования сайта при изменении ширины экрана. назначает, что размер экрана сайта будет соответствовать реальным размерам устройства, у соврмеменных мобильников реальная матрица телефонов сильно меньше 1920 px-->
+    	<title>registration form</title>
+    	<link rel="icon" href="favicon.ico"> <!--здесь подключается фавиконка-->
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
+		<link rel="stylesheet" href="./css/style_home.css">
+	</head>
+    <body>
+        <div class="conteiner">
+            <h1>
+                calculate price
+            </h1>
+            <form action="" method="post">
+                <input type="text" class="form-control" name="login" id="login" placeholder="enter login">
+                <input type="text" class="form-control" name="name" id="name" placeholder="enter name">
+                <button>
+                    registration
+                </button>
+            </form>
+        </div>
+    </body>
+</html>  
+       
